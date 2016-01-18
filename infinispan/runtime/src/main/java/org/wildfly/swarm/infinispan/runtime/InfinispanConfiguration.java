eb45f355624e9b9839dc08e3d82c65d49c86b657
Heiko Braun <ike.braun@googlemail.com>
2016-01-18 14:51:04 +0100

    Align bob's and heiko's extension changes

--- infinispan/runtime/src/main/java/org/wildfly/swarm/infinispan/runtime/InfinispanConfiguration.java
+++ infinispan/runtime/src/main/java/org/wildfly/swarm/infinispan/runtime/InfinispanConfiguration.java
@@ -15,13 +15,8 @@
  */
 package org.wildfly.swarm.infinispan.runtime;
 
-import org.jboss.dmr.ModelNode;
 import org.wildfly.swarm.container.runtime.MarshallingServerConfiguration;
 import org.wildfly.swarm.infinispan.InfinispanFraction;
-
-import java.util.Optional;
-
-import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.*;
 
 /**
  * @author Lance Ball
@@ -39,11 +34,4 @@ public class InfinispanConfiguration extends MarshallingServerConfiguration<Infi
         return InfinispanFraction.createDefaultFraction();
     }
 
-    @Override
-    public Optional<ModelNode> getExtension() {
-        ModelNode node = new ModelNode();
-        node.get(OP_ADDR).add(EXTENSION, "org.jboss.as.clustering.infinispan");
-        node.get(OP).set(ADD);
-        return Optional.of(node);
-    }
 }
